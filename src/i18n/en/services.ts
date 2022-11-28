d89ba4b51e74ccf538d7068c0cf334d3852c80f7
Peter Škríba <peter.skriba@gmail.com>
2022-11-29 00:28:34 +0100

    fix(7-i18n): add missing translation

--- src/i18n/en/services.ts
+++ src/i18n/en/services.ts
@@ -2,13 +2,12 @@ export default {
   help: {
     title: 'Our Services',
     body: 'We enlighten your future and open the door to a bright future with renewable and green energy. Our team consists of experienced professionals who would be happy to work with you from the beginning to the end of your project.',
-    // TODO: missing translation
     list: [
-      'Inštalácia fotovoltaických zariadení a výstavba solárnych parkov',
-      'Financovanie a partnerstvo pri budovaní fotovoltaických zariadení a solárnych parkov',
-      'Konzultácie a poradenstvo v oblasti energetiky a obnoviteľných zdrojov',
-      'Zabezpečenie a dodávka komponentov na výstavbu fotovoltaických zariadení',
-      'Pomoc a podpora pri vykonávaní administratívnych úkonov',
+      'Installation of photovoltaic installations and construction of solar parks',
+      'Financing and partnership when building photovoltaic installations and solar parks',
+      'Consulting and counseling in the field of energetics and renewable sources',
+      'Provision and supply of components for the construction of photovoltaic installations',
+      'Help and support with administrative tasks',
     ],
   },
 }

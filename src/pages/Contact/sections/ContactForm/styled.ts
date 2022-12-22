1ff0e08f6cd4800309769d88e3b435932a55ba5c
Peter Škríba <peter.skriba@gmail.com>
2022-12-22 11:18:07 +0100

    fix: contact map

--- src/pages/Contact/sections/ContactForm/styled.ts
+++ src/pages/Contact/sections/ContactForm/styled.ts
@@ -6,13 +6,13 @@ export const responsiveImage = css`
   align-self: flex-start;
 
   @media (min-width: ${({ theme }) => theme.breakpoints.desktop}px) {
-    margin-top: -140px;
-    margin-left: -20px;
+    margin-top: -110px;
+    margin-left: -15px;
     width: 105%;
   }
 
   @media (min-width: ${({ theme }) => theme.breakpoints.largeDesktop}px) {
-    margin-top: -150px;
-    margin-left: -50px;
+    margin-top: -130px;
+    margin-left: -10px;
   }
 `
